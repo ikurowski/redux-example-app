ba1ba79d0407ed1646390c09ff890626d3609d93
ikurowski <igorkurowski@gmail.com>
2023-01-10 23:56:30 +0100

    add store

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
+
 function App() {
-  return <div> </div>;
+  return (
+    <button
+      type="button"
+      onClick={() => {
+        store.dispatch({
+          type: 'COUNTER',
+        });
+        console.log(store.getState().counter);
+      }}
+    >
+      +1
+    </button>
+  );
 }
 
 export default App;
