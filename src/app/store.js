a002c22a0eb0de01511fd4cdc71bb0e7157282e9
ikurowski <igorkurowski@gmail.com>
2023-01-12 22:29:31 +0100

    add cartSlice

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authSlice from '../features/authSlice';
+import cartSlice from '../features/cartSlice';
 
 const store = configureStore({
-  reducer: authSlice,
+  reducer: {
+    auth: authSlice,
+    cart: cartSlice,
+  },
 });
 export default store;
