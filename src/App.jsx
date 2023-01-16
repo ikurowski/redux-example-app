720827e1e52453e704b1e803c2ca50d731826ec9
ikurowski <igorkurowski@gmail.com>
2023-01-16 21:48:51 +0100

    statusSlice done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
-import React, { useState } from 'react';
+/* eslint-disable object-curly-newline */
+/* eslint-disable object-curly-spacing */
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Alert } from '@mui/material';
 import Header from './components/Header';
 import Main from './components/Main';
 import LoginModal from './components/LoginModal';
+import { putRequestStatus, statusEnum } from './features/statusSlice';
+
+let isInitial = true;
 
 function App() {
   const [showModal, setShowModal] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
+  const cart = useSelector((state) => state.cart);
+  const putStatus = useSelector((state) => state.status.putStatus);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+    dispatch(putRequestStatus(cart));
+  }, [cart, dispatch]);
+
+  useEffect(() => {
+    setShowAlert(true);
+    const timer = setTimeout(() => {
+      setShowAlert(false);
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, [putStatus]);
 
   const closeModal = () => {
     setShowModal(false);
@@ -14,9 +41,67 @@ function App() {
     setShowModal(true);
   };
 
+  let statusAlert;
+  switch (putStatus) {
+    case statusEnum.pending:
+      statusAlert = (
+        <Alert
+          sx={{
+            position: 'fixed',
+            top: '10px',
+            left: '50%',
+            transform: 'translateX(-50%)',
+            width: '25%',
+            zIndex: '1000',
+          }}
+          severity="info"
+        >
+          Sending cart data, please wait...
+        </Alert>
+      );
+      break;
+    case statusEnum.fulfilled:
+      statusAlert = (
+        <Alert
+          sx={{
+            position: 'fixed',
+            top: '10px',
+            left: '50%',
+            transform: 'translateX(-50%)',
+            width: '25%',
+            zIndex: '1000',
+          }}
+          severity="success"
+        >
+          Cart data updated successfully!
+        </Alert>
+      );
+      break;
+    case statusEnum.rejected:
+      statusAlert = (
+        <Alert
+          sx={{
+            position: 'fixed',
+            top: '10px',
+            left: '50%',
+            transform: 'translateX(-50%)',
+            width: '25%',
+            zIndex: '1000',
+          }}
+          severity="error"
+        >
+          Error sending cart data!
+        </Alert>
+      );
+      break;
+    default:
+      statusAlert = null;
+  }
+
   return (
     <>
       <Header openModal={openModal} />
+      {showAlert && statusAlert}
       <Main />
       <LoginModal showModal={showModal} closeModal={closeModal} />
     </>
--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authSlice from '../features/authSlice';
 import cartSlice from '../features/cartSlice';
+import statusSlice from '../features/statusSlice';
 
 const store = configureStore({
   reducer: {
     auth: authSlice,
     cart: cartSlice,
+    status: statusSlice,
   },
 });
 export default store;
